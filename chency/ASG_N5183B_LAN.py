70c30931797db4e234f0b2388bdadf2f52f964f1
takehuge <takehuge@users.noreply.github.com>
2018-09-25 15:20:43 +0800

    Initial commit

---
+++ chency/ASG_N5183B_LAN.py
@@ -0,0 +1,50 @@
+from visa import ResourceManager
+from time import time, sleep
+from numpy import arange
+from colorama import Fore, Back, init
+init(autoreset=True)
+
+start = time()
+rm = ResourceManager()
+
+ip = "169.254.0.1"
+# asg = rm.open_resource("TCPIP0::169.254.157.31::inst0::INSTR")
+asg = rm.open_resource("TCPIP0::%s::INSTR" %ip)
+
+asg.read_termination = '\n'
+asg.timeout = 8000   # milisec
+asg.write("*RST")
+
+who = asg.query("*IDN?")
+print("I am %s" %who)
+
+ip = asg.query(":SYSTem:COMMunicate:LAN:IP?")
+print("IP: %s" %ip)
+
+feq = asg.query(":FREQ?")
+print("frequency: %sGHz" %(float(feq)/1e9))
+
+start_freq = float(input(Back.WHITE + Fore.BLUE + "Enter Starting Frequency (GHz): "))
+stop_freq = float(input(Back.WHITE + Fore.BLUE + "Enter Last Frequency (GHz): "))
+N = int(input(Back.WHITE + Fore.BLUE + "How many points: "))
+
+i = 1
+for freq in arange(start_freq, stop_freq, (stop_freq - start_freq)/N):
+    asg.write(":FREQ %sGHz" %freq)
+    feq = asg.query(":FREQ?")
+    sleep(1)
+    print(Fore.YELLOW + "%s. frequency %s" %(i, feq))
+    i += 1
+
+asg.write(":POW -10dBm")
+asg.write("OUTP 0")    #1 is ON  0 is OFF
+
+
+
+
+
+
+
+duration = time() - start
+asg.close()
+print("Done after %ss" %duration)
---
+++ chency/Scanetwork.py
@@ -0,0 +1,26 @@
+import subprocess
+import platform
+platinfo = platform.system()
+print("This platform is %s" %platinfo)
+
+IPlist, active = [], []
+for i in range(10):
+    IPlist.append("169.254.0." + str(i))
+
+if platinfo == "Windows":
+    pass
+elif platinfo == "Darwin":
+    for ip in IPlist:
+        p = subprocess.Popen(["ping", "-c", "1", "-W" "1", ip], stdout=subprocess.PIPE)
+        msg = [i for i in p.stdout]
+        a = 0
+        for msg in msg:
+            b = int("ttl" in str(msg))
+            a += b
+        print("Scanning %s", ip)
+        if a == 1:
+            active.append(ip)
+
+print("Active IP:")
+for active in active:
+    print(active)
